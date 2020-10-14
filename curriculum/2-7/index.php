9c9d4f08242c0895dd67bac1a20eb58460d4039e
akari <amamhrhr@gmail.com>
2020-10-14 22:23:15 +0900

    2-7追加
    2-7記載もれ追記

--- curriculum/2-7/index.php
+++ curriculum/2-7/index.php
@@ -1,5 +1,7 @@
 <?php
+$colors = ["red" => "赤", "blue" => "青", "green" => "緑"];
 $color = ["red" => "赤", "blue" => "青", "green" => "緑"];
 $color ["yellow"] = "黄色";
 
+var_dump($colors);
 var_dump($color);
