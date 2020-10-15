0c33abc4ac9984c895c1addd913db52195bbf017
akari <amamhrhr@gmail.com>
2020-10-16 00:05:38 +0900

    2-11課題提出
    2-11の作成

---
+++ curriculum/2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_GET['my_name'];
+$mail = $_GET['mail'];
+$password = $_GET['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
